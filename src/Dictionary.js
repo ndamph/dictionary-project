3c9df16d024a92cc60d095d870f7f961ea0792a4
Nadine D <158780700+ndamph@users.noreply.github.com>
2024-08-15 16:00:50 -0500

    Created event listeners to enable search functionality

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  const [keyword, setKeyword] = useState(null);
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`Fetching definition of ${keyword}`);
+  }
+
+  function handleChange(event) {
+    setKeyword(event.target.value);
+  }
   return (
     <div className="Dictionary">
-      <form>
-        <input type="search" placeholder="Enter a word.."></input>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Enter a word.."
+          onChange={handleChange}
+        ></input>
         <input type="submit" value="Search"></input>
       </form>
     </div>
