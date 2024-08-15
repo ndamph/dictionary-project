0c2363eaa645995d8534b258e3447b9461cd7384
Nadine D <158780700+ndamph@users.noreply.github.com>
2024-08-15 15:36:01 -0500

    Created a search form

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,13 @@
 import React from "react";
+import "./Dictionary.css";
 
 export default function Dictionary() {
-  return <div className="Dictionary"></div>;
+  return (
+    <div className="Dictionary">
+      <form>
+        <input type="search" placeholder="Enter a word.."></input>
+        <input type="submit" value="Search"></input>
+      </form>
+    </div>
+  );
 }
