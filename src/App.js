1581fc506fff4aec626baf1c5d7e9144fc89c9fa
Nadine D <158780700+ndamph@users.noreply.github.com>
2024-08-15 15:29:56 -0500

    Styled App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
-  return <div className="App">Testing</div>;
+  return (
+    <div className="App">
+      <div className="container text-center">
+        <header>Dictionary App</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Coded by Nadine Damphousse</footer>
+      </div>
+    </div>
+  );
 }
