c38bcf6906b69c9411432b4d88406cc4d1f6d186
Nadine D <158780700+ndamph@users.noreply.github.com>
2024-08-16 10:47:48 -0500

    Rendering the definition and example of each meaning

--- src/Meanings.js
+++ src/Meanings.js
@@ -2,5 +2,19 @@ import React from "react";
 
 export default function Meanings(props) {
   console.log(props.code);
-  return <div className="Meanings"></div>;
+  return (
+    <div className="Meanings">
+      {props.code.meanings.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <h5>{definition.partOfSpeech}</h5>
+            <p>
+              {definition.definition}
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
