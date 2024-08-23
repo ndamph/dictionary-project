d0fad20020eded5495e279a5bc9150d1f59f2391
Nadine D <158780700+ndamph@users.noreply.github.com>
2024-08-23 15:04:30 -0500

    Updated styling and added background image

--- src/Meanings.js
+++ src/Meanings.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import Synonyms from "./Synonyms";
 import Examples from "./Examples";
+import "./Meanings.css";
 
 export default function Meanings(props) {
   return (
     <div className="Meanings">
       {props.code.meanings.map(function (definition, index) {
         return (
-          <div key={index}>
+          <div className="definition-container" key={index}>
             <h5>{definition.partOfSpeech}</h5>
             <p>
               {definition.definition}
