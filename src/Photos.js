60cad0be66f74c5af34d5fc3e5ef03946dd2f7e8
Nadine D <158780700+ndamph@users.noreply.github.com>
2024-08-25 09:31:11 -0500

    Added alt to img element

--- src/Photos.js
+++ src/Photos.js
@@ -12,6 +12,7 @@ export default function Photos(props) {
                   key={index}
                   src={photo.src.landscape}
                   className="img-fluid"
+                  alt="photo"
                 />
               </div>
             );
