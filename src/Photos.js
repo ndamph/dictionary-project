b79838a941bda86d6a0fb7192ea1dd1ae04d5a47
Nadine D <158780700+ndamph@users.noreply.github.com>
2024-08-26 12:50:19 -0500

    Styled the photo gallery

--- src/Photos.js
+++ src/Photos.js
@@ -1,19 +1,23 @@
 import React from "react";
 
+import "./Photos.css";
+
 export default function Photos(props) {
+  console.log(props.photos);
   if (props.photos) {
     return (
       <div className="Photos">
         <div className="row">
           {props.photos.map(function (photo, index) {
             return (
-              <div className="col-4">
-                <img
-                  key={index}
-                  src={photo.src.landscape}
-                  className="img-fluid"
-                  alt="dictionary"
-                />
+              <div className="gallery-photo col-4" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt="dictionary"
+                  />
+                </a>
               </div>
             );
           })}
